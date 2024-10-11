ad6b9f58cee7b173ba0562604266ebd55f8c430d
Jessie Chen <jud40322@gmail.com>
2024-10-12 00:06:42 +0800

    fix : 地端路徑問題

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
 chrome.action.onClicked.addListener((tab) => {
     // 當按下擴展按鈕時打開新標籤頁
-    chrome.tabs.create({ url: 'dist/index.html' });
+    chrome.tabs.create({ url: 'dist/index.html#/editor' });
   });
